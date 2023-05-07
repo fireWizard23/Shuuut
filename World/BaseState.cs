d6ed68f02b2f013acf4827ec5e8bc01d02a1e280
Richard <richard232manansala@gmail.com>
2023-05-07 13:40:56 +0800

    Added baseState & stateManager scripts

---
+++ World/BaseState.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Shuuut.World;
+
+public class BaseState<T> where T : struct, Enum
+{
+    protected StateManager<T> stateManager;
+
+    public void Register(StateManager<T> stateManager)
+    {
+        this.stateManager = stateManager;
+    }
+
+    public virtual void OnEnter()
+    {
+        
+    }
+
+    public virtual void OnExit()
+    {
+        
+    }
+
+    public virtual void Ready()
+    {
+        
+    }
+    
+    
+    public virtual void Process(float delta)
+    {
+    }
+
+    public virtual void PhysicsProcess(float delta)
+    {
+    }
+    
+    
+
+}
---
+++ World/StateManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using Godot;
+
+namespace Shuuut.World;
+
+public class StateManager<T> where T : struct, Enum
+{
+    private Dictionary<T, BaseState<T>> allStates;
+    public readonly Node Parent;
+
+    public BaseState<T> CurrentState { get; private set; }
+    public BaseState<T> PreviousState { get; private set; }
+
+    public T CurrentStateEnum{ get; private set; }
+    public T? PreviousStateEnum { get; private set; }
+    
+    public StateManager(Dictionary<T, BaseState<T>> states, Node parent)
+    {
+        Parent = parent;
+
+        allStates = states;
+        int index = 0;
+        foreach (var h in allStates)
+        {
+                
+            h.Value?.Register(this);
+            if (index == 0)
+            {
+                ChangeState(h.Key);
+                index++;
+            }
+        }
+        
+    }
+
+    public void ChangeState(T newState)
+    {
+        PreviousStateEnum = CurrentStateEnum;
+        CurrentStateEnum = newState;
+        PreviousState = CurrentState;
+        CurrentState = allStates[newState];
+        CurrentState?.OnEnter();
+        PreviousState?.OnExit();
+    }
+
+    public void Ready()
+    {
+        foreach (var v in allStates)
+        {
+            v.Value?.Ready();
+        }
+    }
+
+    public void Process(float delta)
+    {
+        CurrentState?.Process(delta);
+    }
+
+    public void PhysicsProcess(float delta)
+    {
+        CurrentState?.PhysicsProcess(delta);
+    }
+    
+
+}
