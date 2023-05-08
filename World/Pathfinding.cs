bf1e8980f2cd92c79041974b9fb53fe88d5c45fd
Richard <richard232manansala@gmail.com>
2023-05-08 19:16:58 +0800

    Added GetClosestPoint for better pathfinding

--- World/Pathfinding.cs
+++ World/Pathfinding.cs
@@ -72,9 +72,22 @@ public partial class Pathfinding : Node
 
     public List<Vector2> GetPath(Vector2 from, Vector2 to)
     {
-	    var fromId = ToId(from);
-	    var toId = ToId(to);
-	    var paths = _aStar.GetIdPath(fromId, toId);
+	    Vector2I? fromId = ToId(from);
+	    Vector2I? toId = ToId(to);
+	    if (_aStar.IsInBoundsv((Vector2I)fromId) && _aStar.IsPointSolid((Vector2I)fromId))
+	    {
+		    fromId = GetClosestId(from);
+	    }
+	    if (_aStar.IsInBoundsv((Vector2I)toId)  && _aStar.IsPointSolid((Vector2I)toId))
+	    {
+		    toId = GetClosestId(to);
+	    }
+
+	    if (fromId is null || toId is null)
+	    {
+		    return new List<Vector2>();
+	    }
+	    var paths = _aStar.GetIdPath((Vector2I)fromId, (Vector2I)toId);
 
 	    return paths.Select(IdToGlobal).ToList();
 
@@ -93,6 +106,50 @@ public partial class Pathfinding : Node
     {
 	    // return (Vector2I)(globalPos / CellSize);
 	    return tileMap.LocalToMap(tileMap.ToLocal(globalPos));
+    }
+
+    public Vector2I? GetClosestId(Vector2 worldPos)
+    {
+	    List<Vector2I> possibleIds = new List<Vector2I>();
+	    var id = ToId(worldPos);
+	    for (int y = -1; y <= 1; y++)
+	    {
+		    for (int x = -1; x <= 1; x++)
+		    {
+			    if ((x == 0 && y == 0))
+			    {
+				    continue;
+			    }
+			    var v = id + new Vector2I(x,y);
+			    if (!_aStar.IsInBoundsv(v) || _aStar.IsPointSolid(v))
+			    {
+				    continue;
+			    } 
+			    
+			    possibleIds.Add(v);
+		    }
+	    }
+
+	    if (possibleIds.Count == 0)
+	    {
+		    return null;
+	    }
+
+	    var closestWorldPoint = IdToGlobal(possibleIds[0]);
+	    for (int i = 1; i < possibleIds.Count; i++)
+	    {
+		    var current = IdToGlobal(possibleIds[i]);
+		    var distanceOfCurrent = worldPos.DistanceSquaredTo(current);
+		    var distanceOfClosest = worldPos.DistanceSquaredTo(closestWorldPoint);
+
+				if (distanceOfClosest > distanceOfCurrent)
+		    {
+			    closestWorldPoint = current;
+		    }
+
+	    }
+
+	    return ToId(closestWorldPoint);
     }
     
     
